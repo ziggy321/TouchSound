87b36fa03d540dbcccbdc03e25b82db4e42cccb0
Hyuneui Hong <hhelitehh@gmail.com>
2020-11-13 18:43:13 +0900

    11/13 temp

--- src/ConvertToFile.js
+++ src/ConvertToFile.js
@@ -3,286 +3,143 @@ import { AudioTrack } from "./AudioTrack.js";
 import { AudioChannel } from "./AudioChannel.js";
 import { RecordAudio } from "./RecordAudio.js";
 
-
 export class ConvertToFile {
-    constructor({fileInput,audioCtx,compress_btn}) {
+  constructor({}) {
     this.fileInput = document.getElementById('audio-file');
     this.audioCtx = new (AudioContext || webkitAudioContext)();
     this.compress_btn = document.getElementById('compress_btn');
-    
-
 
     //Load button listener
     compress_btn.addEventListener("click", function() {
-
-    // check for file
-      if(fileInput.files[0] == undefined) {
-  
-      // Stop the process and tell user they need to upload a file.
-      return false;
+      // check for file
+      if(this.fileInput.files[0] == undefined) {
+        // Stop the process and tell user they need to upload a file.
+        return false;
       }
-  
-    this.reader1 = new FileReader();
-        reader1.onload = function(ev) {
-          
-          // Decode audio
-          audioCtx.decodeAudioData(ev.target.result).then(function(buffer) {
-  
-              // Process Audio
-  
-          });
-        };
-        reader1.readAsArrayBuffer(fileInput.files[0]);
-  
+      let reader1 = new FileReader();
+      reader1.onload = function(ev) {
+        // Decode audio
+        this.audioCtx.decodeAudioData(ev.target.result).then(function(buffer) {
+          // Process Audio
+          this.processAudio(buffer);
+          this.createCompressorNode();
+          this.renderAudio();
+        });
+      };
+      reader1.readAsArrayBuffer(this.fileInput.files[0]);
     }, false);
-    }
-}
+  }
+
   // Process Audio
-    
+  processAudio = buffer => {
     this.offlineAudioCtx = new OfflineAudioContext({
       numberOfChannels: 2,
       length : 44100 * ArrayBuffer.duration,
       sampleRate : 44100,
     });
 
-  // Audio Buffer Source
-    this.soundSource = offlineAudioCtx.createBufferSource();
+    // Audio Buffer Source
+    this.soundSource = this.offlineAudioCtx.createBufferSource();
     this.soundSource.buffer = buffer;
+  }
 
-// Create Compressor Node
-    this.compressor = offlineAudioCtx.createDynamicsCompressor();
+  // Create Compressor Node
+  createCompressorNode = () => {
+    this.compressor = this.offlineAudioCtx.createDynamicsCompressor();
+    this.compressor.threshold.setValueAtTime(-20, this.offlineAudioCtx.currentTime);
+    this.compressor.knee.setValueAtTime(-30, this.offlineAudioCtx.currentTime);
+    this.compressor.ratio.setValueAtTime(5, this.offlineAudioCtx.currentTime);
+    this.compressor.attack.setValueAtTime(.05, this.offlineAudioCtx.currentTime);
+    this.compressor.release.setValueAtTime(.25, this.offlineAudioCtx.currentTime);
+  }
 
-      compressor.threshold.setValueAtTime(-20, offlineAudioCtx.currentTime);
-      compressor.knee.setValueAtTime(-30, offlineAudioCtx.currentTime);
-      compressor.ratio.setValueAtTime(5, offlineAudioCtx.currentTime);
-      compressor.attack.setValueAtTime(.05, offlineAudioCtx.currentTime);
-      compressor.release.setValueAtTime(.25, offlineAudioCtx.currentTime);
+  // Connect nodes to destination
+  renderAudio = () => {
+    this.soundSource.connect(this.compressor);
+    this.compressor.connect(this.offlineAudioCtx.destination);
 
-// Connect nodes to destination
-    soundSource.connect(compressor);
-    compressor.connect(offlineAudioCtx.destination);
-
-    offlineAudioCtx.startRendering().then(function(renderedBuffer) {
-  
-      make_download(renderedBuffer, offlineAudioCtx.length);
-  
+    this.offlineAudioCtx.startRendering().then(function(renderedBuffer) {
+      this.make_download(renderedBuffer, this.offlineAudioCtx.length);
     }).catch(function(err) {
-    // Handle error
-      });
+      // Handle error
+      console.log(err)
+    });
+  }
 
+  // get duration and sample rate
   make_download = (abuffer, total_samples) => {
-
-	// get duration and sample rate
 	  this.duration = abuffer.duration,
 		this.rate = abuffer.sampleRate,
-		this.offset = 0;
+    this.offset = 0;
 
-    this.new_file = URL.createObjectURL(bufferToWave(abuffer, total_samples));
+    const new_file = URL.createObjectURL(this.bufferToWave(abuffer, total_samples));
 
-    this.download_link = document.getElementById("download_link");
+    let download_link = document.getElementById("download_link");
 	  download_link.href = new_file;
-	  this. name = generateFileName();
+	  let name = this.generateFileName();
 	  download_link.download = name;
-
-}
+  }
 
   generateFileName = () => {
-    this. origin_name = fileInput.files[0].name;
-    this. pos = origin_name.lastIndexOf('.');
-    this. no_ext = origin_name.slice(0, pos);
+    const origin_name = this.fileInput.files[0].name;
+    const pos = origin_name.lastIndexOf('.');
+    const no_ext = origin_name.slice(0, pos);
 
     return no_ext + ".compressed.wav";
   }
 
-// Convert an AudioBuffer to a Blob using WAVE representation
+  // Convert an AudioBuffer to a Blob using WAVE representation
   bufferToWave = (abuffer, len) =>{
-    this.numOfChan = abuffer.numberOfChannels,
-    this.length = len * numOfChan * 2 + 44,
-    this.buffer = new ArrayBuffer(length),
-    this.view = new DataView(buffer),
-    this.channels = [], i, sample,
+    const buffer = new ArrayBuffer(length)
+    const numOfChan = abuffer.numberOfChannels
+    const length = len * numOfChan * 2 + 44
+    let channels = [], i, sample
+    this.view = new DataView(buffer)
     this.offset = 0,
     this.pos = 0;
   
     // write WAVE header
-    setUint32(0x46464952);                         // "RIFF"
-    setUint32(length - 8);                         // file length - 8
-    setUint32(0x45564157);                         // "WAVE"
+    this.setUint32(0x46464952);                         // "RIFF"
+    this.setUint32(length - 8);                         // file length - 8
+    this.setUint32(0x45564157);                         // "WAVE"
   
-    setUint32(0x20746d66);                         // "fmt " chunk
-    setUint32(16);                                 // length = 16
-    setUint16(1);                                  // PCM (uncompressed)
-    setUint16(numOfChan);
-    setUint32(abuffer.sampleRate);
-    setUint32(abuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
-    setUint16(numOfChan * 2);                      // block-align
-    setUint16(16);                                 // 16-bit (hardcoded in this demo)
+    this.setUint32(0x20746d66);                         // "fmt " chunk
+    this.setUint32(16);                                 // length = 16
+    this.setUint16(1);                                  // PCM (uncompressed)
+    this.setUint16(numOfChan);
+    this.setUint32(abuffer.sampleRate);
+    this.setUint32(abuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
+    this.setUint16(numOfChan * 2);                      // block-align
+    this.setUint16(16);                                 // 16-bit (hardcoded in this demo)
   
-    setUint32(0x61746164);                         // "data" - chunk
-    setUint32(length - pos - 4);                   // chunk length
+    this.setUint32(0x61746164);                         // "data" - chunk
+    this.setUint32(length - this.pos - 4);                   // chunk length
   
     // write interleaved data
-    for(i = 0; i < abuffer.numberOfChannels; i++)
+    for(let i = 0; i < abuffer.numberOfChannels; i++)
       channels.push(abuffer.getChannelData(i));
   
-    while(pos < length) {
-      for(i = 0; i < numOfChan; i++) {             // interleave channels
-        sample = Math.max(-1, Math.min(1, channels[i][offset])); // clamp
+    while(this.pos < length) {
+      for(let i = 0; i < numOfChan; i++) {             // interleave channels
+        sample = Math.max(-1, Math.min(1, channels[i][this.offset])); // clamp
         sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767)|0; // scale to 16-bit signed int
-        view.setInt16(pos, sample, true);          // write 16-bit sample
-        pos += 2;
+        this.view.setInt16(this.pos, sample, true);          // write 16-bit sample
+        this.pos += 2;
       }
-      offset++                                     // next source sample
+      this.offset++                                     // next source sample
     }
   
     // create Blob
     return new Blob([buffer], {type: "audio/wav"});
-  
-    setUint16 = (data) => {
-      view.setUint16(pos, data, true);
-      pos += 2;
-    }
-  
-    setUint32 = (data) => {
-      view.setUint32(pos, data, true);
-      pos += 4;
-    }
   }
 
-  this. fileInput = document.getElementById('audio-file');
-  this. audioCtx = new (AudioContext || webkitAudioContext)();
-  this. compress_btn = document.getElementById('compress_btn');
-
-// Load button listener
-compress_btn.addEventListener("click", function() {
-
-  // Check for file
-  if(fileInput.files[0] == undefined) {
-
-    // Stop the process and tell user they need to upload a file.
-    return false;
+  setUint16 = (data) => {
+    this.view.setUint16(this.pos, data, true);
+    this.pos += 2;
   }
 
-  this. reader1 = new FileReader();
-	reader1.onload = function(ev) {
-	    
-	    // Decode audio
-	    audioCtx.decodeAudioData(ev.target.result).then(function(buffer) {
-
-		    // Process Audio
-				this.offlineAudioCtx = new OfflineAudioContext({
-				  numberOfChannels: 2,
-				  length: 44100 * buffer.duration,
-				  sampleRate: 44100,
-				});
-
-				// Audio Buffer Source
-				soundSource = offlineAudioCtx.createBufferSource();
-				soundSource.buffer = buffer;
-
-				// Create Compressor Node
-				compressor = offlineAudioCtx.createDynamicsCompressor();
-
-				compressor.threshold.setValueAtTime(-20, offlineAudioCtx.currentTime);
-				compressor.knee.setValueAtTime(30, offlineAudioCtx.currentTime);
-				compressor.ratio.setValueAtTime(5, offlineAudioCtx.currentTime);
-				compressor.attack.setValueAtTime(.05, offlineAudioCtx.currentTime);
-				compressor.release.setValueAtTime(.25, offlineAudioCtx.currentTime);
-
-				// Connect nodes to destination
-				soundSource.connect(compressor);
-				compressor.connect(offlineAudioCtx.destination);
-
-				offlineAudioCtx.startRendering().then(function(renderedBuffer) {
-					
-					make_download(renderedBuffer, offlineAudioCtx.length);
-				
-				}).catch(function(err) {
-				  // Handle error
-				});
-	    });
-	  };
-	  reader1.readAsArrayBuffer(fileInput.files[0]);
-
-}, false);
-
-function make_download(abuffer, total_samples) {
-
-	// set sample length and rate
-	this. duration = abuffer.duration,
-		rate = abuffer.sampleRate,
-		offset = 0;
-
-	// Generate audio file and assign URL
-	this. new_file = URL.createObjectURL(bufferToWave(abuffer, total_samples));
-
-	// Make it downloadable
-	this. download_link = document.getElementById("download_link");
-	download_link.href = new_file;
-	this. name = generateFileName();
-	download_link.download = name;
-}
-
-// Utility to add "compressed" to the uploaded file's name
-generateFileName = () => {
-	this. origin_name = fileInput.files[0].name;
-	this. pos = origin_name.lastIndexOf('.');
-	this. no_ext = origin_name.slice(0, pos);
-
-	return no_ext + ".compressed.wav";
-}
-
-// Convert AudioBuffer to a Blob using WAVE representation
-bufferToWave = (abuffer, len) => {
-	this.numOfChan = abuffer.numberOfChannels,
-	this.length = len * numOfChan * 2 + 44,
-	this.buffer = new ArrayBuffer(length),
-	this.view = new DataView(buffer),
-	this.channels = [], i, sample,
-	this.offset = 0,
-	this.pos = 0;
-
-	// write WAVE header
-	setUint32(0x46464952);                         // "RIFF"
-	setUint32(length - 8);                         // file length - 8
-	setUint32(0x45564157);                         // "WAVE"
-
-	setUint32(0x20746d66);                         // "fmt " chunk
-	setUint32(16);                                 // length = 16
-	setUint16(1);                                  // PCM (uncompressed)
-	setUint16(numOfChan);
-	setUint32(abuffer.sampleRate);
-	setUint32(abuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
-	setUint16(numOfChan * 2);                      // block-align
-	setUint16(16);                                 // 16-bit (hardcoded in this demo)
-
-	setUint32(0x61746164);                         // "data" - chunk
-	setUint32(length - pos - 4);                   // chunk length
-
-	// write interleaved data
-	for(i = 0; i < abuffer.numberOfChannels; i++)
-		channels.push(abuffer.getChannelData(i));
-
-	while(pos < length) {
-		for(i = 0; i < numOfChan; i++) {             // interleave channels
-			sample = Math.max(-1, Math.min(1, channels[i][offset])); // clamp
-			sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767)|0; // scale to 16-bit signed int
-			view.setInt16(pos, sample, true);          // write 16-bit sample
-			pos += 2;
-		}
-		offset++                                     // next source sample
-	}
-
-	// create Blob
-	return new Blob([buffer], {type: "audio/wav"});
-
-	setUint16 = (data) => {
-		view.setUint16(pos, data, true);
-		pos += 2;
-	}
-
-	setUint32 = (data) => {
-		view.setUint32(pos, data, true);
-		pos += 4;
-	}
+  setUint32 = (data) => {
+    this.view.setUint32(this.pos, data, true);
+    this.pos += 4;
+  }
 }
--- src/SaveAudio.js
+++ src/SaveAudio.js
@@ -8,49 +8,4 @@ export class SaveAudio{
         $saveAudio.addEventListener('click', putFileName);
     }
 
-    readyToSaveFile(audioContext, audioComplete, filename){
-        console.log(audioComplete);
-        this.filename = filename;
-
-        let destinationForSavingFile = audioContext.createMediaStreamDestination();
-        let mediaStream = destinationForSavingFile.stream;
-        audioComplete.connect(destinationForSavingFile);
-
-        const options = {mimeType: 'video/webm;codecs=vp9'};
-        let mediaRecorder = new MediaRecorder(mediaStream, options);
-        mediaRecorder.ondataavailable = event => this.pushDataToChunks(event);
-
-        mediaRecorder.start();
-        if (audioContext.state === 'suspended') {
-            audioContext.resume();
-        }
-        audioComplete.start();
-
-        mediaRecorder.stop();
-        audioComplete.stop();
-    }
-
-    pushDataToChunks(event){
-        console.log(event.data);
-        if (event.data.size > 0) {
-            console.log("data-available");
-            console.log(this);
-            this.audioChunks.push(event.data);
-            console.log(this.audioChunks);
-            this.createFile();
-        }
-    }
-
-    createFile() {
-        let blob = new Blob(this.audioChunks, { type: "audio/mpeg" });
-        let url = URL.createObjectURL(blob);
-        let a = document.createElement("a");
-        //document.body.appendChild(a);
-        a.style = "display: none";
-        a.href = url;
-        a.download = this.filename;
-        a.click();
-        //window.URL.revokeObjectURL(url);
-    }
-
 }
